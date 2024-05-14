3a71521abcf78c1417c3b70c84c1360aeeddf5f9
nokian5500 <nokian5500@gmail.com>
2024-05-14 14:57:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-GITHUB_TOKENS = ['X', 'X', 'X', 'X', 'X']
+GITHUB_TOKENS = ['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', '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', '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', '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', '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', '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', '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', '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', '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', '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', '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', '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', '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', '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']
 GITHUB_URL_FILE = 'rawGitUrls.txt'
 GITHUB_API_URL = 'https://api.github.com/search/code?q='
 GITHUB_API_COMMIT_URL = 'https://api.github.com/repos/'
@@ -6,8 +6,8 @@ GITHUB_SEARCH_PARAMS = '&sort=indexed&o=desc'
 GITHUB_BASE_URL = 'https://github.com'
 GITHUB_MAX_RETRY = 10
 DISCORD_WEBHOOKURL = 'https://discordapp.com/api/webhooks/7XXXXXXXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXX'
-SLACK_WEBHOOKURL = 'https://hooks.slack.com/services/TXXXXXXXX/BXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXX'
+SLACK_WEBHOOKURL = 'https://webhook-test.com/2168eec3701f28264137ec3c3ed2da5f'
 TELEGRAM_CONFIG = {
-    "token": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    "chat_id": -999999999999999
+    "token": "7093015059:AAEozvolUw8vnMveIntiumVxpIzJe2Wj5jQ",
+    "chat_id": "@gitgrab277"
 }
